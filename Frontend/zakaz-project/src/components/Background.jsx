6c296d57b5aa8144c23d9e0b97eec7394c6bfa7a
Bastard12 <rus.akopyan03@mail.ru>
2023-04-10 23:13:42 +0300

    Background

---
+++ Frontend/zakaz-project/src/components/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Background = () => {
+    return (
+        <div
+            style={{
+                backgroundColor: 'white',
+                width: '100px',
+                height: '100px'
+            }}
+        />
+    );
+}
+
+export default App;
