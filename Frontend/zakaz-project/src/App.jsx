c8d5f7bcf1bf387551fc9d6527979b3a784c3ae9
Starman-in-the-sky <ms.practice@yandex.ru>
2023-04-11 21:13:11 +0300

    Making search 1

--- Frontend/zakaz-project/src/App.jsx
+++ Frontend/zakaz-project/src/App.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import './styles/App.css';
 import Background from "./components/Background";
 import Navbar from "./components/Navbar";
+import Search from "./components/Search"
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <Background/>
-                <Navbar/>
-                YESSSSS
+                <Search/>
             </div>
         );
     }
--- Frontend/zakaz-project/src/components/Search.jsx
+++ Frontend/zakaz-project/src/components/Search.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import "../styles/Search.css"
+import * as mdb from 'mdb-ui-kit';
 
 const Search = (props) => {
-
+    return(
+        <div className='search-box'>
+            <input
+                type="text"
+                size=""
+            />
+        </div>
+    );
 }
 
 export default Search;
