b0bcf1c6718ede33378c30a6e0742d3df80f1a41
Starman-in-the-sky <ms.practice@yandex.ru>
2023-04-11 15:26:49 +0300

    Starting 1

---
+++ Frontend/zakaz-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/App.css';
+import Background from "./components/Background";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Background/>
+                YESSSSS
+            </div>
+        );
+    }
+}
+
+export default App;
--- Frontend/zakaz-project/src/components/Background.jsx
+++ Frontend/zakaz-project/src/components/Background.jsx
@@ -4,10 +4,12 @@ const Background = () => {
     return (
         <div
             style={{
-                backgroundColor: 'white',
+                backgroundColor: 'green',
                 width: '100px',
                 height: '100px'
             }}
         />
     );
 }
+
+export default Background;
