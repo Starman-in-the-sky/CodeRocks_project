21857b03283a53a0411fcf7d406ccdf495f3a58c
hacha <hachatryan.artem.2002@gmail.com>
2023-04-11 13:38:50 +0300

    added Registration

---
+++ Backend/Workers/Registration.py
@@ -0,0 +1,25 @@
+from Backend.Client.sqlite3_client import SQLiteClient
+from Backend.Workers.exacuter import Users, Employee, Employer
+
+database_client = SQLiteClient("users_cunem.db")
+
+class Registration:
+    def __init__(self, password: str, FIO: str, email = "", phone_number=""):
+        self.email = email
+        self.FIO = FIO
+        self.password = password
+        self.phone_number = phone_number
+
+    def check(self):
+        if self.email == "" and self.phone_number == "":
+            return False
+
+        elif self.password == None:
+            return False
+
+        return True
+
+    def add_user(self):
+        u = Users(database_client=database_client)
+        u.setup()
+        u.create_user(email=self.email, phone_number=self.phone_number, password=self.password, FIO=self.FIO)
