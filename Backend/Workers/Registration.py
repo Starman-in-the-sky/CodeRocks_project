4cf785ad557743244676127e49fa37f009e908c8
hacha <hachatryan.artem.2002@gmail.com>
2023-04-11 20:35:33 +0300

    adapted to hosted database

--- Backend/Workers/Registration.py
+++ Backend/Workers/Registration.py
@@ -1,10 +1,11 @@
 from Backend.Client.sqlite3_client import SQLiteClient
 from Backend.Workers.exacuter import Users, Employee, Employer
 
-database_client = SQLiteClient("pg2.sweb.ru/hachatryan")
+database_client = SQLiteClient(dbname='hachatryan', user='hachatryan', password='367878Artem', host='pg2.sweb.ru')
 
 class Registration:
-    def __init__(self, password: str, FIO: str, email = "", phone_number=""):
+    def __init__(self, user_id: int, email: str, password: str, FIO: str, phone_number=""):
+        self.user_id = user_id
         self.email = email
         self.FIO = FIO
         self.password = password
@@ -13,7 +14,6 @@ class Registration:
     def check(self):
         if self.email == "" and self.phone_number == "":
             return False
-
         elif self.password == None:
             return False
 
@@ -22,4 +22,4 @@ class Registration:
     def add_user(self):
         u = Users(database_client=database_client)
         u.setup()
-        u.create_user(email=self.email, phone_number=self.phone_number, password=self.password, FIO=self.FIO)
+        u.create_user(user_id= self.user_id, email=self.email, phone_number=self.phone_number, password=self.password, FIO=self.FIO)
