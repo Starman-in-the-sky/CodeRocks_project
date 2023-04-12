8ab68d9a730c2fe82ae6c39bba056b2b5b8d8f03
DeniRadionov <deniradionov190202@gmail.com>
2023-04-12 13:04:42 +0300

    search

--- Backend/App.py
+++ Backend/App.py
@@ -1,21 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 from Client.sqlite3_client import SQLiteClient
 from Workers.exacuter import Users, Employee, Employer, EmployeeResponses, EmployerResponses
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_msearch import Search
-
-search = Search()
-
+from flask import request
+import psycopg2
 
 app = Flask(__name__)
-search.init_app(app)
+app.secret_key = "caircocoders-ednalan"
 
-@app.route('/search')
-def search():
-    keyword = request.args.get('q')
-    search_posts = Post.query.msearch(keyword, fields=['title', 'content'], limit=6)
-    return render_template('post/search.html', search_posts=search_posts)
+conn = psycopg2.connect(dbname='hachatryan', user='hachatryan', password='367878Artem', host='pg2.sweb.ru')
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/ajaxlivesearch", methods=["POST", "GET"])
+def ajaxlivesearch():
+    cur = conn.cursor()
+    if request.method == 'POST':
+        search_word = request.form['query']
+        print(search_word)
+        if search_word == '':
+            query = "SELECT * from employers ORDER BY employer_id"
+            cur.execute(query)
+            employer = cur.fetchall()
+        else:
+            cur.execute('SELECT * FROM employers WHERE vacancy LIKE %(vacancy)s', {'vacancy': '%{}%'.format(search_word)})
+            numrows = int(cur.rowcount)
+            employer = cur.fetchall()
+            print(numrows)
+    return jsonify({'htmlresponse': render_template('response.html', employer=employer, numrows=numrows)})
 
 if __name__ == "__main__":
      app.run(debug=True)
